8db71cbf71c1cb578488f2b38513ca9d01d829df
Dmitriy Bulgakov <4irtim@gmail.com>
2023-04-25 14:50:59 +0200

    fead: add button modules, keyboard class basic elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './styles.sass';
-import { keyboard } from './keyboard/keyboard';
+import keyboard from './keyboard/keyboard';
 
 keyboard();
--- src/keyboard/keyboard.js
+++ src/keyboard/keyboard.js
@@ -1,5 +1,49 @@
+import elementFab from './func';
+
+class Keyboard {
+  constructor(language = 'ru') {
+    this.lang = language; // lang = ru || en
+
+    // Main elements
+    this.container = elementFab('main', ['container']);
+    this.caption = elementFab('h1', ['container__caption'], 'Виртуальная клавиатура');
+    this.text = elementFab('textarea', ['container__text']);
+    this.key = elementFab('section', ['keyboard']);
+    this.info = elementFab('h2', ['container__info'], 'Переключение языка Alt + Shift');
+  }
+
+  render(parentElement = document.body) {
+    // Render all elements
+    this.root = parentElement;
+    this.container.append(this.caption, this.text, this.key, this.info);
+    this.root.append(this.container);
+  }
+}
+
 const keyboard = () => {
-  document.body.innerHTML = 'TEST';
+  const key = new Keyboard();
+  key.render();
 };
 
-export { keyboard };
+export default keyboard;
+
+// Structure
+// container
+//  caption
+//  textarea
+//  keyboard
+//   button (main style)
+//   TEMPLATE
+//    div
+//   small button (letters, numbers, arrows, CTRL, WIN, ALT)
+//   del button
+//   tab button
+//   enter, right shift button
+//   capslock, left shift button
+//   backspace button
+//   space button
+//   MODIFICATORS
+//    colored buttons (system)
+//    hover style
+//    active style
+//  language chenge info
