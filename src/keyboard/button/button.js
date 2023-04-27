b24082e2ca28109a65430469d5203fd26d86e5b0
Dmitriy Bulgakov <4irtim@gmail.com>
2023-04-27 16:24:43 +0200

    feat: add cursor position handler

--- src/keyboard/button/button.js
+++ src/keyboard/button/button.js
@@ -11,14 +11,6 @@ class Button {
     const cl = this.key.class ? this.key.class : this.key.type + this.key.en;
     return elementFab('div', ['basic-key', this.key.type, cl], this.key[this.lang]);
   }
-
-  handleEvent(shift, caps) {
-    let i = this.lang;
-    if (shift || caps) {
-      i += 'S';
-    }
-    this.element.innerText = this.key[i];
-  }
 }
 
 export default Button;
--- src/keyboard/keyboard.js
+++ src/keyboard/keyboard.js
@@ -3,12 +3,15 @@ import Button from './button/button';
 import keys from './keys';
 
 // Textarea size
-const textCols = '95';
+const textCols = '98';
 const textRows = '20';
 
 class Keyboard {
   constructor(language = 'ru') {
     this.lang = language; // lang = ru || en
+    this.shift = false;
+    this.caps = false;
+    this.cursor = 0;
 
     // Main elements
     this.container = elementFab('main', ['container']);
@@ -19,6 +22,10 @@ class Keyboard {
     this.key = elementFab('section', ['keyboard']);
     this.info = elementFab('h2', ['container__info'], 'Переключение языка Alt + Shift');
 
+    this.mouseDownEvent = this.key.addEventListener('click', (event) => (this.mouseDownHandler(event, this)));
+    this.textCliclEvent = this.text.addEventListener('click', (event) => (this.textClickHandler(event, this)));
+
+    // DEVELOPMENT
     this.keyDownEvent = window.addEventListener('keydown', (event) => (this.kewDownHandler(event, this)));
   }
 
@@ -26,18 +33,38 @@ class Keyboard {
     // Render all elements
     this.root = parentElement;
     this.buttons = this.renderButtons(keys);
-    this.key.append(...this.buttons.map(v => v.render()));
+    this.key.append(...this.buttons.map((v) => v.render()));
     this.container.append(this.caption, this.text, this.key, this.info);
     this.root.append(this.container);
   }
 
   renderButtons(keyArray) {
     const arr = Array(keyArray.length);
-    keyArray.map((v, i) => {
+    keyArray.forEach((v, i) => {
       arr[i] = new Button(v, this.lang);
     });
     return arr;
   }
+
+  mouseDownHandler(event, env) {
+    const type = event.target.classList[1];
+    const { innerText } = event.target;
+    event.target.classList.toggle('active'); // Add animation class
+    if (type === 'letter' || type === 'number') {
+      const { value } = this.text;
+      // const { selectionStart } = this.text;
+      // this.text.value += innerText;
+      this.text.value = value.slice(0, this.cursor) + innerText + value.slice(this.cursor);
+      this.cursor += 1;
+    }
+    event.target.classList.toggle('active'); // Remove animation class
+  }
+
+  textClickHandler(event, env) {
+    const e = env;
+    e.cursor = this.text.selectionStart;
+  }
+
   // DEVELOP FUNCTION
   kewDownHandler(event, env) {
     env.text.value += event.key;
@@ -50,24 +77,3 @@ const keyboard = () => {
 };
 
 export default keyboard;
-
-// Structure
-// container
-//  caption
-//  textarea
-//  keyboard
-//   button (main style)
-//   TEMPLATE
-//    div
-//   small button (letters, numbers, arrows, CTRL, WIN, ALT)
-//   del button
-//   tab button
-//   enter, right shift button
-//   capslock, left shift button
-//   backspace button
-//   space button
-//   MODIFICATORS
-//    colored buttons (system)
-//    hover style
-//    active style
-//  language chenge info
